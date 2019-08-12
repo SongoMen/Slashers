21546ca5258eb0d99762ccf462f5e1174f00a020
SongoMen <songomengaming@gmail.com>
2019-08-12 15:21:52 +0200

    s

--- main.js
+++ main.js
@@ -165,36 +165,31 @@ function load() {
 
 function sideText() {
   TweenMax.to(".line", 0.5, {
-    height: "0%",
-    top: "100%"
-  });
+      height: "0%",
+      top: "100%"
+  })
   TweenMax.to(".counter span", 0.5, {
-    marginBottom: -45,
-    delay: 0.5
-  });
+      marginBottom: -45,
+      delay: 0.5,
+  })
   TweenMax.set(".line", {
-    top: 0,
-    delay: 1
-  });
+      top: 0,
+      delay: 1
+  })
   TweenMax.to(".line", 0.5, {
-    ease: Elastic.easeOut.config(1, 1),
-    height: "50%",
-    delay: 1
-  });
+      ease: Elastic.easeOut.config(1, 1),
+      height: "100%",
+      delay: 1
+  })
   TweenMax.set(".counter span", {
-    marginBottom: 45,
-    delay: 1
-  });
-  TweenMax.staggerTo(
-    ".counter span",
-    0.5,
-    {
+      marginBottom: 45,
+      delay: 1
+  })
+  TweenMax.staggerTo(".counter span", 0.5, {
       ease: Elastic.easeOut.config(1, 1),
       marginBottom: 0,
-      delay: 1.5
-    },
-    0.03
-  );
+      delay: 1.5,
+  }, 0.03)
 }
 
 $(window).on("mousewheel", function(event) {
@@ -203,8 +198,46 @@ $(window).on("mousewheel", function(event) {
     page: "+= 1",
     delay: 1,
     onComplete: function() {
-      projectIn();
+      load();
     }
   });
+   if (event.deltaY == 1 && !transitioning) {
+    projectOut();
+    if (page > 0) {
+        TweenMax.set(window, {
+            page: "-= 1",
+            delay: 1,
+            onComplete: function() {
+                projectIn();
+            }
+        })
+    } else {
+        TweenMax.set(window, {
+            page: 5 - 1,
+            delay: 1,
+            onComplete: function() {
+                projectIn();
+            }
+        })
+    }
+  }
 });
+
+$(".topbar__logo").on("mouseenter", function() {
+  TweenMax.to(".mouse-track .outer-circle", 0.5, {
+      height: 80,
+      width: 80,
+      strokeWidth: 2,
+      autoAlpha: 0
+  })
+}).on("mouseleave", function() {
+  TweenMax.to(".mouse-track .outer-circle", 0.5, {
+      height: 18,
+      width: 18,
+      strokeWidth: 2,
+      autoAlpha: 1
+  })
+})
+
+
 load();
